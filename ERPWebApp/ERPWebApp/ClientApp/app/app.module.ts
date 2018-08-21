a8d6d89e6b3cafa7b8ec8c52b39e20d8ec9c8e9a
amar10063 <amar10063@yahoo.co.in>
2018-08-21 15:19:00 +0530

    html to angular all navbar

--- ERPWebApp/ERPWebApp/ClientApp/app/app.module.ts
+++ ERPWebApp/ERPWebApp/ClientApp/app/app.module.ts
@@ -1,4 +1,5 @@
-
+/// <reference path="components/hr/hr-area/hr-area.component.ts" />
+/// <reference path="components/hr/hr-area/hr-area.component.ts" />
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -12,6 +13,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HrAreaComponent } from './components/hr/hr-area/hr-area.component';
 import { PayrollAreaComponent } from './components/payroll/payroll-area/payroll-area.component';
+import { ProductionAreaComponent } from './components/production/production-area/production-area.component';
+import { InventoryAreaComponent } from './components/inventory/inventory-area/inventory-area.component';
+import { GeneralLedgerAreaComponent } from './components/general-ledger/general-ledger-area/general-ledger-area.component';
+import { PurchaseAreaComponent } from './components/purchase/purchase-area/purchase-area.component';
+import { SaleAreaComponent } from './components/sales/sale-area/sale-area.component';
+import { FixedAssetsAreaComponent } from './components/fixed-assets/fixed-assets-area/fixed-assets-area.component';
 @NgModule({
     declarations: [
         AppComponent,
@@ -21,24 +28,29 @@ import { PayrollAreaComponent } from './components/payroll/payroll-area/payroll-
         FooterComponent,
         HeaderComponent,
         HrAreaComponent,
-        PayrollAreaComponent
+        PayrollAreaComponent,
+        ProductionAreaComponent,
+        InventoryAreaComponent,
+        GeneralLedgerAreaComponent,
+        PurchaseAreaComponent,
+        SaleAreaComponent,
+        FixedAssetsAreaComponent,
     ],
     imports: [
         CommonModule,
         HttpModule,
         FormsModule,
         RouterModule.forRoot([
-            { path: '', redirectTo: 'login', pathMatch: 'full' },
-            { path: 'login', component: LoginComponent },
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'Contact-form', component: ContactComponent },
             { path: 'footer', component: FooterComponent },
-            { path: 'hr-area', component: HrAreaComponent }, 
-            { path: 'payroll-area', component: PayrollAreaComponent }, 
-            { path: 'production', component: ProductionAreaComponent }, 
-            { path: 'inventory', component: InventoryAreaComponent }, 
-            { path: 'general-ledger', component: GeneralLedgerAreaComponent }, 
-            { path: 'purchase', component: PurchaseAreaComponent }, 
+            { path: 'hr-area', component: HrAreaComponent },
+            { path: 'payroll-area', component: PayrollAreaComponent },
+            { path: 'production', component: ProductionAreaComponent },
+            { path: 'inventory', component: InventoryAreaComponent },
+            { path: 'general-ledger', component: GeneralLedgerAreaComponent },
+            { path: 'purchase', component: PurchaseAreaComponent },
             { path: 'sale', component: SaleAreaComponent },
             { path: 'fixed-assets', component: FixedAssetsAreaComponent },
             { path: '**', redirectTo: 'home' }
