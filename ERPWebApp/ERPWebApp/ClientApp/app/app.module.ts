cd4cb97f8d7ac6c183d585725c7bbc33ef483d52
amar10063 <amar10063@yahoo.co.in>
2018-11-26 10:44:06 +0530

    amar commit on restart project

--- ERPWebApp/ERPWebApp/ClientApp/app/app.module.ts
+++ ERPWebApp/ERPWebApp/ClientApp/app/app.module.ts
@@ -1,5 +1,4 @@
-/// <reference path="components/hr/hr-area/hr-area.component.ts" />
-/// <reference path="components/hr/hr-area/hr-area.component.ts" />
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -12,6 +11,7 @@ import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HrAreaComponent } from './components/hr/hr-area/hr-area.component';
+import { OrganizationComponent } from './components/hr/organization/organization.component';
 import { PayrollAreaComponent } from './components/payroll/payroll-area/payroll-area.component';
 import { ProductionAreaComponent } from './components/production/production-area/production-area.component';
 import { InventoryAreaComponent } from './components/inventory/inventory-area/inventory-area.component';
@@ -55,6 +55,7 @@ import { ResetPasswordComponent } from './components/reset-password/reset-passwo
             { path: 'Contact-form', component: ContactComponent },
             { path: 'footer', component: FooterComponent },
             { path: 'hr-area', component: HrAreaComponent },
+            //{ path: 'hr-area/organization', component: OrganizationComponent },
             { path: 'payroll-area', component: PayrollAreaComponent },
             { path: 'production', component: ProductionAreaComponent },
             { path: 'inventory', component: InventoryAreaComponent },
